b604b2072ca82190e45bb41c4f2cc60ba02e0e73
k2wl <kartik838@yahoo.co.in>
2013-11-17 03:08:58 +0530

    change device name from s2evp to baffin

--- full_baffin.mk
+++ full_baffin.mk
@@ -2,11 +2,11 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/s2vep/s2vep.mk)
+$(call inherit-product, device/samsung/baffin/baffin.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := s2vep
-PRODUCT_NAME := full_s2vep
+PRODUCT_DEVICE := baffin
+PRODUCT_NAME := full_baffin
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := GT-I9105P
+PRODUCT_MODEL := GT-I9082
 PRODUCT_MANUFACTURER := samsung
