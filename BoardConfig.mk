c5643b3cd29b764f06ae1b9db456ac3ee935aaf4
k2wl <kartik838@yahoo.co.in>
2013-11-22 10:35:38 +0530

    trying to fix wifi again

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,12 +10,12 @@ USE_CAMERA_STUB := true
 TARGET_KERNEL_SOURCE := kernel/samsung/baffin
 TARGET_KERNEL_CONFIG := cyanogenmod_baffin_defconfig
 
-# Wifi
+# Connectivity - Wi-Fi
 BOARD_WLAN_DEVICE_REV := bcm4334
-WIFI_DRIVER_FW_PATH_STA     := "/system/etc/wifi/bcmdhd_sta.bin_b2"
-WIFI_DRIVER_FW_PATH_AP      := "/system/etc/wifi/bcmdhd_apsta.bin_b2"
-WIFI_DRIVER_MODULE_ARG      := "firmware_path=/system/etc/wifi/bcmdhd_sta.bin_b2 nvram_path=/system/etc/wifi/nvram_net.txt"
-WIFI_DRIVER_MODULE_AP_ARG   := "firmware_path=/system/etc/wifi/bcmdhd_apsta.bin_b2 nvram_path=/system/etc/wifi/nvram_net.txt"
+WIFI_DRIVER_FW_PATH_STA := "/system/etc/wifi/bcmdhd_sta.bin_b2"
+WIFI_DRIVER_FW_PATH_AP := "/system/etc/wifi/bcmdhd_apsta.bin_b2"
+WIFI_DRIVER_MODULE_ARG := "firmware_path=/system/etc/wifi/bcmdhd_sta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
+WIFI_DRIVER_MODULE_AP_ARG := "firmware_path=/system/etc/wifi/bcmdhd_apsta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/baffin/bluetooth
